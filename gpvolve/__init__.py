6055fc75f8a50346446c8c39fe3e7ef3c360a20d
Luis Perez <lperezmo@uoregon.edu>
2021-12-15 12:41:48 -0600

    Updated readme, removed old binder links, removed versions from setup.py that were preventing automated tests from running, and added a new figure

--- gpvolve/__init__.py
+++ gpvolve/__init__.py
@@ -1,6 +1,6 @@
 # Import the main module in this package
 from gpvolve.__version__ import __version__
-from gpvolve.base import build_transition_matrix, apply_fitness_function
+from gpvolve.base import build_transition_matrix, apply_fitness_function, to_greedy
 from gpvolve import simulate
 from gpvolve import utils
 from gpvolve import pyplot
@@ -8,5 +8,4 @@ from gpvolve import cluster
 from gpvolve import markov
 from gpvolve import analysis
 from gpvolve import phenotype_to_fitness
-# Import fixation models with simpler names
-# import gpvolve.markov.base._generate_tmatrix.fixation as fixation_models
+
--- gpvolve/base.py
+++ gpvolve/base.py
@@ -1,7 +1,6 @@
 from gpvolve.check import gpm_sanity
 from gpvolve.utils import flatten_neighbors
 from gpvolve.markov import base
-from gpvolve.phenotype_to_fitness import linear, sigmoid, exponential, step
 import numpy as np
 
 
@@ -94,3 +93,40 @@ def build_transition_matrix(gpm, fixation_model='moran', **params):
     return t
 
 
+def to_greedy(transition_matrix):
+    """
+    Turn transition matrix into 'greedy' transition matrix. Only the step with
+    the highest positive fitness difference is allowed (prob. = 1), all other
+    steps are not permitted (prob. = 0).
+    Parameters
+    ----------
+    transition_matrix : 2D numpy.ndarray.
+        Transition matrix where the highest value T(i->j) per row i should
+        correspond to the step s(i->j) where j is the neighbor of genotype i
+        with the highest fitness. Can be obtained using the 'ratio' fixation
+        function, where transition probability T(i->j) is simply the ratio of
+        fitness j over fitness i.
+    Returns
+    -------
+    M : 2D numpy.ndarray.
+        Transition matrix corresponding to a 'greedy random walk' on the
+        genotype-phenotype map.
+    References
+    ----------
+    de Visser JA, Krug J. 2014. Empirical fitness landscapes and the
+    predictability of evolution. Nature Reviews Genetics 15:480–490.
+    """
+    T = transition_matrix.copy()
+    # Remove self-looping probability/matrix diagonal = 0
+    np.fill_diagonal(T, 0)
+
+    # Get column index of max value for each row.
+    indices = np.argmax(T, axis=1)
+    # Set index pointer (check scipy.sparse.csr_matrix documentation).
+    indptr = np.array(range(T.shape[0] + 1))
+    # Since there is only on possible greedy step per row, it is assigned probability of 1.
+    data = np.ones(T.shape[0])
+
+    M = csr_matrix((data, indices, indptr), shape=T.shape).toarray()
+
+    return M
